53317ed93a38dd0a34334024f8d81c0aba910e27
root <taekg.park@gmail.com>
2022-01-10 23:05:25 +0900

    create backed project

---
+++ TaskAgile/backend/src/main/kotlin/com/taskagile/app/TaskAgileApplication.kt
@@ -0,0 +1,11 @@
+package com.taskagile.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskAgileApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaskAgileApplication>(*args)
+}
