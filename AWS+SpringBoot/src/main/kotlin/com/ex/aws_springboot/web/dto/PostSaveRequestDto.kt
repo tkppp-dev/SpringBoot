afc72bdcffb1ce5e6b72b5c1b337e2d8899d109b
root <taekg.park@gmail.com>
2022-01-28 03:30:29 +0900

    postSaveDto property modify

--- AWS+SpringBoot/src/main/kotlin/com/ex/aws_springboot/web/dto/PostSaveRequestDto.kt
+++ AWS+SpringBoot/src/main/kotlin/com/ex/aws_springboot/web/dto/PostSaveRequestDto.kt
@@ -3,7 +3,7 @@ package com.ex.aws_springboot.web.dto
 import com.ex.aws_springboot.domain.post.Post
 
 data class PostSaveRequestDto(
-    private val title: String,
+    val title: String,
     val content: String,
     val author: String? = null
 ) {
